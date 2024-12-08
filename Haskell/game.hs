79ae3bc5dddde145b1741eaa8a6db1ded16c72f0
Mateusz Łukasiewicz <01178556@pw.edu.pl>
2024-12-08 03:53:49 +0100

    Added random items generating

--- Haskell/game.hs
+++ Haskell/game.hs
@@ -2,10 +2,13 @@ import Data.List (intercalate)
 import System.Random (randomRIO)
 
 type Room = String
+type Item = String
 
 data GameState = GameState {
-    currentRoom :: Room
-} deriving (Show)
+    currentRoom :: Room,
+    holding :: [Item],
+    items :: [(Item, Room)]
+}
 
 rooms :: [(Room, [Room])]
 rooms = [
@@ -20,11 +23,55 @@ rooms = [
     ("Cafeteria", ["Upper Engine", "Weapons", "Admin Room", "Storage"]),
     ("Oxygen", ["Shields"]),
     ("Navigation", ["Shields"]),
-    ("Weapons", ["Cafeteria, Shields"]),
+    ("Weapons", ["Cafeteria", "Shields"]),
     ("Admin Room", ["Cafeteria"])]
+
 
 startRooms :: [Room]
 startRooms = ["Reactor", "Security", "Medbay", "Lower Engine", "Storage", "Upper Engine"]
+
+
+initial_items :: [(Item, [Room])]
+initial_items = [
+    ("encyclopedia", startRooms),
+    ("flashlight", startRooms),
+    ("wrench", ["Electrical"]),
+    ("plushie", ["Storage"]),
+    ("empty canister", ["Storage"]),
+    ("pokemon card", ["Storage"]),
+    ("petrol canister", ["Reactor", "Lower Engine", "Storage", "Upper Engine"]),
+    ("gas canister", ["Reactor", "Lower Engine", "Storage", "Upper Engine"]),
+    ("rope", ["Storage"]),
+    ("carton", ["Storage"]),
+    ("lightbulbs", ["Storage"]),
+    ("shotgun", ["Weapons"]),
+    ("v1 access card", startRooms),
+    ("v2 access card", []),
+    ("medical raport", [])]
+
+
+randomizeItemLocation :: [(Item, [Room])] -> IO [(Item, Room)]
+randomizeItemLocation [] = return []
+randomizeItemLocation ((item, rooms):xs) = do
+    room <- if null rooms
+            then return ""
+            else do 
+                idx <- randomRIO (0, length rooms - 1)
+                return (rooms !! idx)
+    rest <- randomizeItemLocation xs
+    return ((item, room): rest)
+
+    
+generateGameState :: IO GameState
+generateGameState = do
+    items <- randomizeItemLocation initial_items
+    idx <- randomRIO (0, length startRooms - 1)
+    startRoom <- return (startRooms !! idx)
+    return GameState {
+        currentRoom = startRoom,
+        holding = [],
+        items = items
+    }
 
 move :: Room -> GameState -> GameState
 move targetRoom state =
@@ -49,6 +96,7 @@ gameLoop :: GameState -> IO ()
 gameLoop state = do
     printLines ["You are in: " ++ currentRoom state]
     printLines ["Available rooms: " ++ intercalate(", ") neighbors]
+    printLines ["Available items: " ++ intercalate(", ") itemsInRoom]
     cmd <- readCommand
     case words cmd of
         ("go" : "to" : rest) -> do
@@ -63,10 +111,10 @@ gameLoop state = do
     neighbors = case lookup (currentRoom state) rooms of
         Just ns -> ns
         Nothing -> []
+    itemsInRoom = [item | (item, room) <- items state, room == currentRoom state]
 
 
 main = do
-    startRoom <- randomRIO (0, length startRooms - 1)
-    let initialState = GameState (startRooms !! startRoom)
+    initialState <- generateGameState
     gameLoop initialState
 
